32a3e3d12ac23a9a9eed42f04b05ab9422814d99
Guilherme Mello <guilhermecfmello@gmail.com>
2024-07-22 18:53:57 -0300

    Create artist model.

--- guessicbe/guessicbeapp/models.py
+++ guessicbe/guessicbeapp/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    img_src = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    debut = models.IntegerField()
+    members = models.IntegerField()
+    popularity = models.IntegerField()
+    gender = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    city_flag = models.CharField(max_length=100)
+    city_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
