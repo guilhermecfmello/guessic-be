7de9926ff8bb98478357b7f6d059513508cc3085
Guilherme Mello <guilhermecfmello@gmail.com>
2024-07-22 19:42:47 -0300

    Format search result.

--- guessicbe/guessicbeapp/views.py
+++ guessicbe/guessicbeapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from rest_framework import status
 
 from guessicbeapp.models import Artist
@@ -6,5 +6,10 @@ from guessicbeapp.models import Artist
 
 def search_artist_by_name(request):
     artist_name = request.GET.get('name', '')
-    artists_found = Artist.objects.filter(name__icontains=artist_name)
-    return HttpResponse(artists_found, content_type='application/json', status=status.HTTP_200_OK)
+    artists = Artist.objects.filter(name__icontains=artist_name)
+
+    response_data = [
+        {"id": str(artist.id), "name": artist.name} for artist in artists
+    ]
+
+    return JsonResponse(response_data, safe=False, status=status.HTTP_200_OK)
